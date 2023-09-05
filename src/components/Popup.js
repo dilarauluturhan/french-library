c36fd51cfa176b18d8df65e79531ce9fce844c1b
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-09-05 22:04:59 +0300

    fix: popup mobile responsive bug fix

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -10,10 +10,10 @@ const Popup = () => {
     return (
         isPopupOpen && (
             <div className='font-nunito fixed top-0 left-0 w-full h-full bg-popup z-10'>
-                <div className='bg-white w-2/3 h-1/3 sm:w-96 sm:h-96 border border-transparent rounded-xl absolute top-1/4 left-16 sm:top-1/4 sm:left-39'>
+                <div className='bg-white w-2/3 h-2/5 sm:w-96 sm:h-96 border border-transparent rounded-xl absolute top-1/4 left-16 sm:top-1/4 sm:left-39'>
                     <h1 className='text-2xl sm:text-4xl font-bold text-center mt-12'>Bonjour!</h1>
                     <p className=' text-md sm:text-xl text-center font-semibold mt-4'>It is an open source project prepared by Dilara Uluturhan and is waiting for the support of the developers. Enjoy your work!</p>
-                    <button className='text-2xl sm:text-3xl font-semibold bg-transparent hover:bg-gray-200 border-2 rounded-lg border-black w-20 h-10 sm:w-24 sm:h-14 absolute left-24 sm:left-36 top-3/4 sm:top-64' onClick={closePopup}>OK🥐</button>
+                    <button className='text-2xl sm:text-3xl font-semibold bg-transparent hover:bg-gray-200 border-2 rounded-lg border-black w-20 h-10 sm:w-24 sm:h-14 absolute left-20 sm:left-36 top-3/4 sm:top-64' onClick={closePopup}>OK🥐</button>
                 </div>
             </div>
         )
